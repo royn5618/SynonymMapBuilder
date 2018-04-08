4a123a57cf426e8d393a1dc21fd4149f171f6d7e
royn5618 <33963924+royn5618@users.noreply.github.com>
2018-04-08 20:12:42 +0100

    Initial Commit

---
+++ src/TRECResourceBuild/ExtractFrequentwords.py
@@ -0,0 +1,81 @@
+'''
+Created on 15 Mar 2018
+
+@author: Owner
+'''
+import numpy as np
+import pandas as pd
+import os
+from pandas import ExcelWriter
+from pandas import DataFrame
+from nltk.probability import FreqDist
+import nltk
+from bs4 import BeautifulSoup
+from rope.base.resources import Folder
+root_path = "C:/Users/Owner/eclipse-workspace/TRECResourceBuild/input_docs"
+from nltk.tokenize import RegexpTokenizer
+
+my_list = []
+tokenizer = RegexpTokenizer(r'\w+')
+
+TRECfiles = os.listdir(root_path)
+for folders in TRECfiles:
+    print(folders)
+    
+    if "fb" in folders:
+        
+        TRECfdocs = os.listdir(root_path + '/' + folders)
+        for doc in TRECfdocs:
+            print(doc)
+            if "fb" in doc:
+                file = open(root_path + '/' + folders + '/' + doc , "r")
+                raw = file.readlines()
+                raw = " ".join(raw)
+                soup = BeautifulSoup(raw, "lxml")
+                all_text = soup.text
+                tokens = tokenizer.tokenize(all_text)
+                freq = FreqDist(tokens)
+                for each in freq.most_common(100):
+                    my_list.append(each[0])
+        print(my_list)
+    if "ft" in folders:
+        TRECfdocs = os.listdir(root_path + '/' + folders)
+        
+        for doc in TRECfdocs:
+            print(doc)
+            TRECfsubdocs = os.listdir(root_path + '/' + folders + '/' + doc)
+            
+            for subdoc in TRECfsubdocs:
+                print(subdoc)
+                if "ft" in file:
+                    file = open(root_path + '/' + folders + '/' + doc + '/' + subdoc , "r")
+                    raw = file.readlines()
+                    raw = " ".join(raw)
+                    soup = BeautifulSoup(raw, "lxml")
+                    all_text = soup.text
+                    tokens = tokenizer.tokenize(all_text)
+                    freq = FreqDist(tokens)
+                    for each in freq.most_common(100):
+                        my_list.append(each[0])
+    print(my_list)  
+    if "la" in folders:
+        TRECfdocs = os.listdir(root_path + '/' + folders)
+        
+        for doc in TRECfdocs:
+            print(doc)
+            if "la" in doc:
+                file = open(root_path + '/' + folders + '/' + doc , "r")
+                raw = file.readlines()
+                raw = " ".join(raw)
+                soup = BeautifulSoup(raw, "lxml")
+                all_text = soup.text
+                print(all_text)
+                tokens = tokenizer.tokenize(all_text)
+                freq = FreqDist(tokens)
+                for each in freq.most_common(100):
+                    my_list.append(each[0])
+    print(my_list)
+unique_set = set(my_list)          
+print("Printing.......")
+with open('stopwords.txt', 'w') as f:
+    print(unique_set, file=f)
---
+++ src/TRECResourceBuild/generateSynonymMap.py
@@ -0,0 +1,75 @@
+'''
+Created on 7 Apr 2018
+
+@author: Owner
+'''
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk.tag import StanfordNERTagger
+from nltk.corpus import words
+from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+
+st = StanfordNERTagger('C:/Users/Owner/eclipse-workspace/TopicModelling/stanford-ner-2018-02-27/classifiers/english.muc.7class.distsim.crf.ser.gz',
+                       'C:/Users/Owner/eclipse-workspace/TopicModelling/stanford-ner-2018-02-27/stanford-ner.jar',
+                       encoding='utf-8')
+
+file_path = "C:/Users/Owner/eclipse-workspace/TRECResourceBuild/src/TRECResourceBuild/stopwords.txt"
+
+
+token_list = []
+final_list = []
+
+#tokenise without punctuations
+tokenizer = RegexpTokenizer(r'\w+')
+
+file = open(file_path, "r")
+lines = file.readlines()
+tokens = tokenizer.tokenize(lines[0])
+
+#for some document numbers appeared - To remove document numbers
+for token in tokens:
+    if "LA" not in token:
+        token_list.append(token)
+classified_text = st.tag(token_list)
+
+#No synonym for people names - to filter persons using NER tagger
+for text in classified_text:
+    if 'PERSON' not in text[1]:
+        final_list.append(text[0])
+tagged_text = nltk.pos_tag(final_list)
+
+#To filter numbers
+fresh_list = []
+for text in tagged_text:
+    if 'CD' not in text[1]:
+        fresh_list.append(text[0])
+        
+#Clean the list by keeping only the english words and removing stopwprds
+stop_Words = set(stopwords.words('english'))
+english_words = set(nltk.corpus.words.words())
+set_words = set(fresh_list)
+legitimate_words = english_words.intersection(set_words) - stop_Words
+
+#remove alphabets from the collection
+for eachword in legitimate_words.copy():
+    if len(eachword) == 1:
+        legitimate_words.remove(eachword)
+#print(legitimate_words)
+#Finally build the map using wordnet
+string_builder = []
+for word in legitimate_words:
+    synsets = wn.synsets(word)
+    synonyms = []
+    for syn in synsets:
+        for each in syn.lemma_names():
+            synonyms.append(each)
+    set_syns = set(synonyms)
+    syn_string = ','.join(set_syns)
+    string_builder.append(syn_string + '\n')
+#print(string_builder)
+
+#Save to file
+with open('synonyms.txt', 'w') as f:
+    print(string_builder, file=f)
+print("Done...")
